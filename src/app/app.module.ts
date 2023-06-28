316d508e8f36e1efd1f5c11f0c7534a078ecb35f
MartinLugo <Martin.kuk@regionaliza.com>
2023-06-27 22:40:07 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+
+import { RouterModule } from '@angular/router';
+
+const routes =[
+  { path: 'home', component: HomeComponent},
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
